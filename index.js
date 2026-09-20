b020e5d8276dad6256f4b132c06d67222342eb40
1andonlyunknown050-dotcom <1andonlyunknown050@gmail.com>
2026-09-20 09:39:20 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online: ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.mentions.has(client.user)) {
+    await message.reply("Hey! 👋 You pinged me!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
